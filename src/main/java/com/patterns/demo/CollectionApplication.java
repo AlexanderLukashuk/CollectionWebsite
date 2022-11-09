eb8232d290a70578063ff132ed3aa42e962003ac
AlexanderLukashuk <upiter465@gmail.com>
2022-11-09 14:43:24 +0600

    Commited non-important codes in the CollectionApplication file

--- src/main/java/com/patterns/demo/CollectionApplication.java
+++ src/main/java/com/patterns/demo/CollectionApplication.java
@@ -9,22 +9,23 @@ import org.springframework.jdbc.core.JdbcTemplate;
 import java.sql.SQLException;
 
 @SpringBootApplication
-public class CollectionApplication implements CommandLineRunner {
+//public class CollectionApplication implements CommandLineRunner {
+public class CollectionApplication {
 
-	@Autowired
-	private JdbcTemplate jdbcTemplate;
+//	@Autowired
+//	private JdbcTemplate jdbcTemplate;
 
 	public static void main(String[] args) {
 		SpringApplication.run(CollectionApplication.class, args);
 	}
 
-	@Override
-	public void run(String... args) throws Exception {
-		String sql = "insert into books (name, price, features) values ('In Search of Lost Time', 500, '{\"Limited Collection\"}'";
-
-		int rows = jdbcTemplate.update(sql);
-		if (rows > 0) {
-			System.out.println("A new row has been inserted");
-		}
-	}
+//	@Override
+//	public void run(String... args) throws Exception {
+//		String sql = "insert into books (name, price, features) values ('In Search of Lost Time', 500, '{\"Limited Collection\"}'";
+//
+//		int rows = jdbcTemplate.update(sql);
+//		if (rows > 0) {
+//			System.out.println("A new row has been inserted");
+//		}
+//	}
 }
